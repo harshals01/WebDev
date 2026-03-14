253666044374b8958d2651c863aee6b7fd8a2389
Harshal <146689301+harshals01@users.noreply.github.com>
2026-03-14 23:55:42 +0530

    Add files via upload

---
+++ day4/arrayops.js
@@ -0,0 +1,14 @@
+
+let a = ["HTML", "CSS", "JS", "React"];
+let b = ["Node.js", "Expess.js"];
+
+
+let concateArray = a.concat(b);
+
+console.log("Concatenated Array: ", concateArray);
+
+
+let a = ["HTML", "CSS", "JS"];
+
+
+console.log(a.toString());
