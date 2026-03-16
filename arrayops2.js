751fed70fa9888330e00598e4169b18e446a705e
Harshal <146689301+harshals01@users.noreply.github.com>
2026-03-16 23:37:39 +0530

    Add employee object and array concatenation example
    Added employee object and demonstrated salary deletion. Implemented array concatenation of multiple arrays.

---
+++ arrayops2.js
@@ -0,0 +1,18 @@
+let emp = { 
+    firstName: "mohan", 
+    lastName: "Kaur", 
+    salary: 40000
+} 
+
+console.log(delete emp.salary); 
+console.log(emp);
+
+
+//concat op
+
+let a1 = [11, 12, 13];
+let a2 = [14, 15, 16];
+let a3 = [17, 18, 19];
+
+let newArr = a1.concat(a2, a3);
+console.log(newArr);
