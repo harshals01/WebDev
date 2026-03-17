feee4ef12da8d48d22313a00a46b9abcf5b490df
Harshal <146689301+harshals01@users.noreply.github.com>
2026-03-17 23:54:17 +0530

    Implement comparison operations in comparisonops.js
    Add comparisons for strict and loose equality, null, and strings.

---
+++ day5/comparisonops.js
@@ -0,0 +1,19 @@
+// strict and loose equality
+
+console.log( null == undefined);
+console.log( null === undefined);
+
+console.log("-----------");   
+
+// null comparison
+
+console.log(null > 0);   
+console.log(null < 1);   
+console.log(null >= 0);  
+console.log(null == 0);  
+
+//test
+console.log(0 > null);   
+console.log("01 " > 10);
+console.log("apple" < "banana");
+
